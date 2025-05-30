346b1f47b18731ef8670c79814e72779c118aaa5
Markos9 <pascualmarcos96@gmail.com>
2025-05-30 13:13:09 +0200

    feature: pipes and i/o error reedirects

--- myshell.c
+++ myshell.c
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
 
 char buf[1024];
 tline *line;
@@ -32,6 +34,120 @@ int command_exists(const char *cmd)
 	free(path_dup);
 	// No existe
 	return 0;
+}
+
+void pipe_commands(tline *line)
+{
+	int i;
+	int n = line->ncommands;
+	int pipes[n - 1][2];
+
+	for (i = 0; i < n - 1; i++)
+	{
+		if (pipe(pipes[i]) == -1)
+		{
+			perror("pipe error");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		if (command_exists(line->commands[i].argv[0]) == 0)
+		{
+			fprintf(stderr, "%s: No se encuentra el mandato\n", line->commands[i].argv[0]);
+			return;
+		}
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		pid_t pid = fork();
+		if (pid < 0)
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+		else if (pid == 0)
+		{
+			// Reedirigir entrada de archivo
+			if (i == 0 && line->redirect_input != NULL)
+			{
+				int fd = open(line->redirect_input, O_RDONLY);
+				if (fd < 0)
+				{
+					fprintf(stderr, "%s: Error. %s\n", line->redirect_input, strerror(errno));
+					exit(EXIT_FAILURE);
+				}
+				dup2(fd, STDIN_FILENO);
+				close(fd);
+			}
+
+			// Reedirigir salida a archivo
+			if (i == n - 1 && line->redirect_output != NULL)
+			{
+				int fd = open(line->redirect_output, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+				if (fd < 0)
+				{
+					fprintf(stderr, "%s: Error. %s\n", line->redirect_output, strerror(errno));
+					exit(EXIT_FAILURE);
+				}
+				dup2(fd, STDOUT_FILENO);
+				close(fd);
+			}
+
+			// Reedirigir error y salida estandar
+			if (line->redirect_error != NULL)
+			{
+				int fd = open(line->redirect_error, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+				if (fd < 0)
+				{
+					fprintf(stderr, "%s: Error. %s", line->redirect_error, strerror(errno));
+					exit(EXIT_FAILURE);
+				}
+				dup2(fd, STDERR_FILENO);
+				close(fd);
+			}
+
+			// Reedirigir entrada (evitando 1 comando)
+			if (i > 0)
+			{
+				dup2(pipes[i - 1][0], STDIN_FILENO);
+			}
+
+			// Reedirigir salida (evitando el ultimo)
+			if (i < n - 1)
+			{
+				dup2(pipes[i][1], STDOUT_FILENO);
+			}
+
+			// Cerrar pipes en el hijo
+			for (int j = 0; j < n - 1; j++)
+			{
+				close(pipes[j][0]);
+				close(pipes[j][1]);
+			}
+
+			execvp(line->commands[i].argv[0], line->commands[i].argv);
+			perror("execvp");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	for (i = 0; i < n - 1; i++)
+	{
+		close(pipes[i][0]);
+		close(pipes[i][1]);
+	}
+
+	if (!line->background)
+	{
+		for (i = 0; i < n; i++)
+		{
+			wait(NULL);
+		}
+	}
 }
 
 int main()
@@ -63,32 +179,83 @@ int main()
 					printf("%s\n", cwd);
 				}
 			}
+			// Comando exit
 			else if (strcmp(line->commands[0].argv[0], "exit") == 0)
 			{
 				printf("Saliendo de msh...\n");
 				exit(0);
 			}
+			// Resto de comandos
 			else
 			{
 				// Comprobar existencia del comando
-				if (command_exists(line->commands[0].argv[0]) == 0)
+
+				if (line->ncommands > 1)
 				{
-					fprintf(stderr, "%s: No se encuentra el mandato\n", line->commands[0].argv[0]);
+					pipe_commands(line);
 				}
 				else
 				{
-					pid_t pid = fork();
-					if (pid == 0)
+					if (command_exists(line->commands[0].argv[0]) == 0)
 					{
-						// Hijo
-						execvp(line->commands[0].argv[0], line->commands[0].argv);
-						perror("execvp error");
-						exit(EXIT_FAILURE);
+						fprintf(stderr, "%s: No se encuentra el mandato\n", line->commands[0].argv[0]);
 					}
 					else
 					{
-						int status;
-						waitpid(pid, &status, 0);
+						pid_t pid = fork();
+						if (pid == 0)
+						{
+							// Hijo
+
+							// Reedirigir entrada de archivo
+							if (line->redirect_input != NULL)
+							{
+								int fd = open(line->redirect_input, O_RDONLY);
+								if (fd < 0)
+								{
+									fprintf(stderr, "%s: Error. %s\n", line->redirect_input, strerror(errno));
+									exit(EXIT_FAILURE);
+								}
+								dup2(fd, STDIN_FILENO);
+								close(fd);
+							}
+
+							// Reedirigir salida a archivo
+							if (line->redirect_output != NULL)
+							{
+								int fd = open(line->redirect_output, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+								if (fd < 0)
+								{
+									fprintf(stderr, "%s: Error. %s\n", line->redirect_output, strerror(errno));
+									exit(EXIT_FAILURE);
+								}
+								dup2(fd, STDOUT_FILENO);
+								close(fd);
+							}
+
+							// Reedirigir error y salida estandar
+							if (line->redirect_error != NULL)
+							{
+								int fd = open(line->redirect_error, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+								if (fd < 0)
+								{
+									fprintf(stderr, "%s: Error. %s", line->redirect_error, strerror(errno));
+									exit(EXIT_FAILURE);
+								}
+								dup2(fd, STDERR_FILENO);
+								close(fd);
+							}
+
+							execvp(line->commands[0].argv[0], line->commands[0].argv);
+							perror("execvp error");
+							exit(EXIT_FAILURE);
+						}
+						else
+						{
+							int status;
+							waitpid(pid, &status, 0);
+						}
 					}
 				}
 			}
